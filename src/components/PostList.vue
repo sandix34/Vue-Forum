1f0fe2598d99c54a73305c2285448e2615cff74d
sandrine <sandrinezocca@gmail.com>
2019-11-07 17:37:53 +0100

    extraire le composant PageThreadShow en composants plus petits

---
+++ src/components/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="post-list">
+    <PostListItem
+     v-for="post in posts"
+     :key="post['.key']"
+     :post="post" 
+    />
+  </div>
+</template>
+
+<script>
+import PostListItem from './PostListItem'
+
+export default {
+  components: {
+    PostListItem
+  },
+  props: {
+    posts: {
+      required: true,
+      type: Array
+    }
+  }
+}
+</script>
---
+++ src/components/PostListItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="post">
+      <div class="user-info">
+        <a href="#" class="user-name">{{user.name}}</a>
+        <a href="#">
+          <img class="avatar-large" :src="user.avatar" alt />
+        </a>
+        <p class="desktop-only text-small">{{userPostCount}} posts</p>
+      </div>
+      <div class="post-content">
+        <div>{{post.text}}</div>
+      </div>
+      <div class="post-date text-faded">{{post.publisheAt}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import sourceData from '@/data'
+
+export default {
+  props: {
+    post: {
+      required: true,
+      type: Object
+    }
+  },
+  computed: {
+    user () {
+      return sourceData.users[this.post.userId]
+    },
+    userPostCount () {
+      return Object.keys(this.user.posts).length
+    }
+  }
+}
+</script>
