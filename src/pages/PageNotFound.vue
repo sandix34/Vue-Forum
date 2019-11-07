dc9e810a739d9a4f93e6d0a81e09439f4c5fff13
sandrine <sandrinezocca@gmail.com>
2019-11-07 15:59:52 +0100

    traitement des pages non trouvées

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="col-full">
+    <h1>Not Found</h1>
+    <p>
+      Ooooops !!!!
+      <router-link :to="{name: 'Home'}">go home instead</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/PageHome'
 import ThreadShow from '@/pages/PageThreadShow'
+import NotFound from '@/pages/PageNotFound'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       name: 'ThreadShow',
       component: ThreadShow,
       props: true
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
     }
   ],
   mode: 'history'
