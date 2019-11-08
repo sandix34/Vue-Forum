67574be2f6a83ce506803b2a344ce2576c58a3de
sandrine <sandrinezocca@gmail.com>
2019-11-08 17:59:26 +0100

    création d'un nouveau post à l'aide d'un formulaire

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,6 +2,21 @@
     <div class="col-large push-top">
       <h1>{{thread.title}}</h1>
       <PostList :posts="posts"/>
+      <form @submit.prevent="addPost">
+        <div class="forum-group">
+          <textarea
+            name="" 
+            id="" 
+            cols="30" 
+            rows="10" 
+            class="form-input"
+            v-model="newPostText"
+            ></textareaname="">
+        </div>
+        <div class="form-action">
+          <button class="btn-blue">Submit post</button>
+        </div>
+      </form>
     </div>
 </template>
 
@@ -21,7 +36,8 @@ export default {
   },
   data () {
     return {
-      thread: sourceData.threads[this.id]
+      thread: sourceData.threads[this.id],
+      newPostText: ''
     }
   },
   computed: {
@@ -29,6 +45,27 @@ export default {
       const postIds = Object.values(this.thread.posts)
       return Object.values(sourceData.posts)
         .filter(post => postIds.includes(post['.key']))
+    }
+  },
+  methods: {
+    addPost () {
+      const postId = 'greatPost' + Math.random()
+
+      const post = {
+        text: this.newPostText,
+        publisheAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: '7uVPJS9GHoftN58Z2MXCYDqmNAh2',
+        '.key': postId
+      }
+
+      this.$set(sourceData.posts, postId, post)
+
+      this.$set(this.thread.posts, postId, postId)
+
+      this.$set(sourceData.users[post.userId].posts, postId, postId)
+
+      this.newPostText = ''
     }
   }
 }
