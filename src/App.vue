4433209e8ac58c0ec077747e291c91847632429d
sandrine <sandrinezocca@gmail.com>
2019-11-12 14:12:23 +0100

    composant à instance unique barre de navigation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <TheNavBar/>
     <div class="container">
       <router-view/>
     </div>
@@ -7,8 +8,12 @@
 </template>
 
 <script>
+import TheNavBar from '@/components/TheNavBar'
+
 export default {
-  name: 'app'
+  components: {
+    TheNavBar
+  }
 }
 </script>
 
