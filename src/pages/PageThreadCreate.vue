fd86998d8c6b6a9c7d3941399ebd12c1448e5368
sandrine <sandrinezocca@gmail.com>
2019-11-13 16:54:37 +0100

    exporter le component ThreadEditor

--- src/pages/PageThreadCreate.vue
+++ src/pages/PageThreadCreate.vue
@@ -1,63 +1,45 @@
 <template>
   <div class="col-full push-top">
     <h1>Create new thread in <i>{{forum.name}}</i></h1>
-    <form @submit.prevent="save">
-      <div class="form-group">
-        <label for="thread_title">Title:</label>
-        <input v-model="title" type="text" id="thread_title" class="form-input" name="title">
-      </div>
-      <div class="form-group">
-        <label for="thread_content">Content:</label>
-        <textarea
-          v-model="text" 
-          id="thread_content" 
-          class="form-input" 
-          name="content" 
-          rows="8" 
-          cols="140"
-        ></textareav-model="text">
-      </div>
-      <div class="btn-group">
-        <button @click.prevent="cancel" class="btn btn-ghost">Cancel</button>
-        <button class="btn btn-blue" type="submit" name="Publish">Publish </button>
-      </div>
-    </form>
+    <ThreadEditor
+      @save="save"
+      @cancel="cancel"
+    />
   </div>
 </template>
 
 <script>
-    export default {
-      props: {
-        forumId: {
-          type: String,
-          required: true
-        }
-      },
-      data () {
-        return {
-          title: '',
-          text: ''
-        }
-      },
-      computed: {
-        forum () {
-          return this.$store.state.forums[this.forumId]
-        }
-      },
-      methods: {
-        save () {
-          // dispatch action
-          this.$store.dispatch('createThread', {
-            forumId: this.forum['.key'],
-            title: this.title,
-            text: this.text
-          }).then(thread => {
-            this.$router.push({name: 'ThreadShow', params: {id: thread['.key']}})
-          })
-        },
-        cancel () {
-          this.$router.push({name: 'Forum', params: {id: this.forum['.key']}})
-        }
-      }
+import ThreadEditor from '@/components/ThreadEditor'
+
+export default {
+  components: {
+    ThreadEditor
+  },
+  props: {
+    forumId: {
+      type: String,
+      required: true
     }
+  },
+  computed: {
+    forum () {
+      return this.$store.state.forums[this.forumId]
+    }
+  },
+  methods: {
+    save ({title, text}) {
+      // dispatch action
+      this.$store.dispatch('createThread', {
+        forumId: this.forum['.key'],
+        title,
+        text
+      }).then(thread => {
+        this.$router.push({name: 'ThreadShow', params: {id: thread['.key']}})
+      })
+    },
+    cancel () {
+      this.$router.push({name: 'Forum', params: {id: this.forum['.key']}})
+    }
+  }
+}
 </script>
