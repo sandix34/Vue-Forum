726fa2fb8b378853a65c7891d89454c5c15344b1
sandrine <sandrinezocca@gmail.com>
2019-11-14 14:09:18 +0100

    Vue.js Custom Prop Validation

--- src/components/PostEditor.vue
+++ src/components/PostEditor.vue
@@ -24,7 +24,19 @@
         required: false
       },
       post: {
-        type: Object
+        type: Object,
+        validator: obj => {
+          const keyIsValid = typeof obj['.key'] === 'string'
+          const textIsValid = typeof obj.text === 'string'
+          const valid = keyIsValid && textIsValid
+          if (!textIsValid) {
+            console.error('😳 The post prop object must include a `text` attribute.')
+          }
+          if (!keyIsValid) {
+            console.error('😳 The post prop object must include a `.key` attribute.')
+          }
+          return valid
+        }
       }
     },
     data () {
