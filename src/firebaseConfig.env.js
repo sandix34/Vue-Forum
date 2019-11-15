71e009680e9a98e2b151655d8a6578c31d27190d
sandrine <sandrinezocca@gmail.com>
2019-11-15 12:41:52 +0100

    install firebase

---
+++ src/firebaseConfig.env.js
@@ -0,0 +1,10 @@
+// Add your web app's Firebase configuration in the .env.js file
+export const firebaseConfig = {
+    apiKey: '---------',
+    authDomain: '--------',
+    databaseURL: '---------',
+    projectId: '--------',
+    storageBucket: '-----------',
+    messagingSenderId: '-----------',
+    appId: '----------'
+  }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import {firebaseConfig} from './.env'
 import App from './App'
 import router from './router'
 import store from '@/store'
@@ -9,6 +13,9 @@ import AppDate from '@/components/AppDate'
 Vue.component('AppDate', AppDate)
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 /* eslint-disable no-new */
 new Vue({
