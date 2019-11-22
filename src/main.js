b9e3bb6d9ac5da6a580e37ebef2c6ae06cf57164
sandrine <sandrinezocca@gmail.com>
2019-11-22 17:24:54 +0100

    utilisation de variables d'environnement

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import Vue from 'vue'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
-import {firebaseConfig} from './.env'
 import App from './App'
 import router from './router'
 import store from '@/store'
@@ -14,8 +13,18 @@ Vue.component('AppDate', AppDate)
 
 Vue.config.productionTip = false
 
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
+  appId: process.env.FIREBASE_APP_ID
+}
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(config)
 
 /* eslint-disable no-new */
 new Vue({
