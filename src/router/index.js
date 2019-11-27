e61c13e5d1d3f342a1a95b85b220c38fd878ccf8
sandrine <sandrinezocca@gmail.com>
2019-11-27 15:24:34 +0100

    Protéger les pages des utilisateurs authentifiés

--- src/router/index.js
+++ src/router/index.js
@@ -37,7 +37,8 @@ const router = new Router({
       path: '/thread/create/:forumId',
       name: 'ThreadCreate',
       component: ThreadCreate,
-      props: true
+      props: true,
+      meta: { requiresAuth: true }
     },
     {
       path: '/thread/:id',
@@ -49,7 +50,8 @@ const router = new Router({
       path: '/thread/:id/edit',
       name: 'ThreadEdit',
       component: ThreadEdit,
-      props: true
+      props: true,
+      meta: { requiresAuth: true }
     },
     {
       path: '/me',
@@ -62,11 +64,13 @@ const router = new Router({
       path: '/me/edit',
       name: 'ProfileEdit',
       component: Profile,
-      props: {edit: true}
+      props: {edit: true},
+      meta: { requiresAuth: true }
     },
     {
       path: '/logout',
       name: 'SignOut',
+      meta: { requiresAuth: true },
       beforeEnter (to, from, next) {
         store.dispatch('signOut')
           .then(() => next({name: 'Home'}))
@@ -80,12 +84,14 @@ const router = new Router({
     {
       path: '/register',
       name: 'Register',
-      component: Register
+      component: Register,
+      meta: { requiresGuest: true }
     },
     {
       path: '/signin',
       name: 'SignIn',
-      component: SignIn
+      component: SignIn,
+      meta: { requiresGuest: true }
     }
   ],
   mode: 'history'
@@ -98,6 +104,13 @@ router.beforeEach((to, from, next) => {
       if (to.matched.some(route => route.meta.requiresAuth)) {
         // protected route
         if (user) {
+          next()
+        } else {
+          next({name: 'SignIn'})
+        }
+      } else if (to.matched.some(route => route.meta.requiresGuest)) {
+        // protected route
+        if (!user) {
           next()
         } else {
           next({name: 'Home'})
